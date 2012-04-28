e60f1b755f2646002e7c9501eca246a5d995df83
Jens Fahnenbruck <jigfox@me.com>
2012-04-28 14:05:24 +0200

    adds json parse for events

--- lib/events/base.rb
+++ lib/events/base.rb
@@ -7,12 +7,31 @@ module Events
     def as_json(*)
       {
         type:   self.class.name.downcase.gsub('events::',''),
-        object: game_object,
+        game_object: game_object,
       }
     end
 
     def to_json(*)
       JSON(as_json)
+    end
+
+    def initialize(opts = {})
+      opts.each do |key, value|
+        public_send "#{key}=", value
+      end
+    end
+
+    def self.parse(json)
+      case json
+      when String
+        parse(JSON(json))
+      when Hash
+        type = json.delete('type').capitalize
+        klass = Events.const_get(type)
+        klass.new json
+      else
+        raise "needs JSON String or Hash"
+      end
     end
   end
 end
