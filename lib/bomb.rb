0c1d510508aa1610b02d55cc2d7f67b0eb4211c2
Moritz Breit <mail@mobr.de>
2012-04-28 18:31:22 +0200

    Use EventMachine time to explode bomb

--- lib/bomb.rb
+++ lib/bomb.rb
@@ -1,10 +1,17 @@
+require 'eventmachine'
 require 'game_object'
 
 class Bomb
   include GameObject
+  include Logging
 
   def initialize(game, coordinates)
     self.game = game
     self.coordinates = coordinates
+    EventMachine::add_timer(3) { explode }
+  end
+
+  def explode
+    info("Booooooooooooooooom")
   end
 end
