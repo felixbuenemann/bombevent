cd188f6a03556d0c49fee723d0818240aa9a892a
Moritz Breit <mail@mobr.de>
2012-04-28 14:11:11 +0200

    Implement simple move in Player class

--- lib/player.rb
+++ lib/player.rb
@@ -1,10 +1,25 @@
+require 'game_object'
+
 class Player
+  include GameObject
+
   def initialize(game)
     @game = game
   end
 
   def move(direction)
-    # TODO
+    case direction
+    when :up
+      coordinates[1] -= 1
+    when :down
+      coordinates[1] += 1
+    when :left
+      coordinates[0] += 1
+    when :right
+      coordinates[0] -= 1
+    end
+
+    @game.send(Events::Move.new())
   end
 end
 
