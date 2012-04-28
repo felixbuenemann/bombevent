baf932b24a5bee4d4ed96ec64d90ed50af9ef83d
Moritz Breit <mail@mobr.de>
2012-04-28 23:50:26 +0200

    Explosion: kill all players moving into an explosion

--- lib/explosion.rb
+++ lib/explosion.rb
@@ -7,13 +7,29 @@ class Explosion
   def initialize(game, coordinates, seconds = 1)
     self.game = game
     self.coordinates = coordinates
+    EventMachine::add_timer(seconds) { delete }
+
+    @subscription_name = game.subscribe { destroy_objects }
+
+    destroy_objects
+  end
+
+  def delete
+    game.unsubscribe(@subscription_name)
+    super
+  end
+
+  def destroy_objects
     game.destroyable_objects_at(*coordinates).each do |object|
       object.delete
     end
-    EventMachine::add_timer(seconds) { delete }
   end
 
   def destroyable?
+    false
+  end
+
+  def solid?
     false
   end
 end
