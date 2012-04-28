99dc606c6d14ed3b49a48b26a55edfe969aa6ebb
Moritz Breit <mail@mobr.de>
2012-04-28 13:44:45 +0200

    Add simple Client initializing and event processing

--- lib/client.rb
+++ lib/client.rb
@@ -6,5 +6,19 @@ class Client
   def initialize(game, websocket)
     info("New connection")
     @game, @websocket = game, websocket
+    @player = Player.new(game)
+    @game.add_player(@player)
+    websocket.onmessage { |msg| process_message(msg) }
+  end
+
+  def process_message(msg)
+    debug("Recieved message: #{msg}")
+    event = Events::Base.parse(msg)
+    case event
+    when Events::Move
+      @player.move(event.direction)
+    when Events::Delete
+      # TODO
+    end
   end
 end
