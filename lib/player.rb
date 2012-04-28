249ffc0370a3ce72976e9a4902d7bde9624f3713
Moritz Breit <mail@mobr.de>
2012-04-28 14:40:35 +0200

    Fix player movement
    * Initialize coordinates
    * Swap left and right
    * Initialize event parameters

--- lib/player.rb
+++ lib/player.rb
@@ -1,25 +1,27 @@
 require 'game_object'
+require 'events/position'
 
 class Player
   include GameObject
 
   def initialize(game)
     @game = game
+    @coordinates = [0, 0]
   end
 
   def move(direction)
-    case direction
+    case direction.to_sym
     when :up
       coordinates[1] -= 1
     when :down
       coordinates[1] += 1
     when :left
-      coordinates[0] += 1
-    when :right
       coordinates[0] -= 1
+    when :right
+      coordinates[0] += 1
     end
 
-    @game.send(Events::Position.new())
+    @game.send(Events::Position.new(self, coordinates))
   end
 end
 
