aa92d21cf58b345cb85c46bfdceace9e6263745e
Jens Fahnenbruck <jigfox@me.com>
2012-04-29 15:41:41 +0200

    bugfix for score event

--- lib/events/score.rb
+++ lib/events/score.rb
@@ -1,13 +1,13 @@
 module Events
   class Score < Base
     attr_accessor :nickname, :player_id, :score
-  end
 
-  def as_json(*)
-    super.merge(
-      nickname:  nickname,
-      player_id: player_id,
-      score:     score,
-    )
+    def as_json(*)
+      super.merge(
+        nickname:  nickname,
+        player_id: player_id,
+        score:     score,
+      )
+    end
   end
 end
